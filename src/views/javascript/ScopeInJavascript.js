02f3d292534ae492b786597c4406f712a03545e2
Alamgir <alamgirkabir45789@gmail.com>
2022-03-12 17:38:57 +0600

    fsdf

---
+++ src/views/javascript/ScopeInJavascript.js
@@ -0,0 +1,21 @@
+name = "kabir";
+console.log(name);
+var name;
+
+var a = 6;
+function myFunc() {
+  a = 4;
+  console.log(a * 2);
+}
+let car = "Volvo";
+console.dir(window);
+myFunc();
+
+let b = 55;
+function Add() {
+  let b = 2;
+
+  return b;
+}
+console.log(Add());
+console.log(b);
